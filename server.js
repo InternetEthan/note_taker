7c13f3b98b087c6c6f66f839a4ecd8cf9756c11c
InternetEthan <internetethan@gmail.com>
2023-10-24 14:52:45 -0500

    reorganized file structure; server.js and key directories in root

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+// Import required modules
+const express = require('express'); // Import the Express framework
+const app = express(); // Create an instance of the Express application
+const path = require('path'); // Import the path module for working with file and directory paths
+const fs = require('fs'); // Import the fs module for working with the file system
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,7 +18,7 @@ app.get('/notes', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+const PORT = process.env.PORT || 3000; // Set the port number for the server
+app.listen(PORT, () => { // Start the server and listen for incoming requests
+    console.log(`Server listening on port ${PORT}`); // Log a message to the console when the server starts
 });
